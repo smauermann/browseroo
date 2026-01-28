829b67a54a11c56330e874ed4e91b638aa171a13
smauermann <github@mauermann.io>
2026-01-28 23:27:56 +0100

    feat: US-005 - Display browser list in menu

--- Browseroo/BrowserooApp.swift
+++ Browseroo/BrowserooApp.swift
@@ -4,7 +4,41 @@ import SwiftUI
 struct BrowserooApp: App {
     var body: some Scene {
         MenuBarExtra("Browseroo", systemImage: "globe") {
-            Text("Browseroo")
+            BrowserMenuView()
         }
+    }
+}
+
+struct BrowserMenuView: View {
+    @State private var browsers: [Browser] = []
+    @State private var defaultBrowserID: String?
+
+    private let browserManager = BrowserManager()
+
+    var body: some View {
+        Group {
+            ForEach(browsers) { browser in
+                Button(action: {
+                    // TODO: Implement browser switching in US-006
+                }) {
+                    HStack {
+                        Image(nsImage: browser.icon)
+                        Text(browser.name)
+                        Spacer()
+                        if browser.bundleIdentifier == defaultBrowserID {
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+            }
+        }
+        .onAppear {
+            refreshBrowserList()
+        }
+    }
+
+    private func refreshBrowserList() {
+        browsers = browserManager.getInstalledBrowsers()
+        defaultBrowserID = browserManager.getDefaultBrowser()?.bundleIdentifier
     }
 }
